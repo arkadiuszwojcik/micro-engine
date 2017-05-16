3bf3a06ca524e5e3b3ce8fde9caa9cf46a0cdef3
Arkadiusz Wojcik <awojcik21@gmail.com>
2017-05-16 19:45:31 +0200

    Add project files.

---
+++ MicroEngine.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicroEngine.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
